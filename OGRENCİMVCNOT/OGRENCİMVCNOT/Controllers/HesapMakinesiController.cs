74d10054a3dd51cf8787df76a7b43a6851f39d12
faysalcudi <faysal.1957@outlook.com>
2021-07-16 11:53:10 +0300

    MVC ile öğrenci not sistemi
    C# dili kullanarak oluşturulmuş öğrenci not sistemi

---
+++ OGRENCİMVCNOT/OGRENCİMVCNOT/Controllers/HesapMakinesiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OGRENCİMVCNOT.Controllers
+{
+    public class HesapMakinesiController : Controller
+    {
+        // GET: HesapMakinesi
+        public ActionResult Index(double sayi1 = 0, double sayi2 = 0)
+        {
+            double sonuctoplam = sayi1 + sayi2;
+            double sonuccıkarım = sayi1 - sayi2;
+            double sonuccarpım = sayi1 * sayi2;
+            double sonucbolum = sayi1 / sayi2;
+
+
+
+            ViewBag.tpl = sonuctoplam;
+            ViewBag.ckr = sonuccıkarım;
+            ViewBag.crp = sonuccarpım;
+            ViewBag.blm = sonucbolum;
+            ViewBag.sayi1=sayi1;
+            ViewBag.sayi2 = sayi2;
+
+            return View();
+        }
+        
+        
+    }
+    
+}
---
+++ OGRENCİMVCNOT/OGRENCİMVCNOT/Controllers/OgrencıController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OGRENCİMVCNOT.Models.EntityFramework;
+
+
+namespace OGRENCİMVCNOT.Controllers
+{
+    public class OgrencıController : Controller
+    {
+        // GET: Ogrencı
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var ogrenciler = db.TBLOGRENCILER.ToList();
+            return View(ogrenciler);
+        }
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKULUPLER.ToList()
+
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KULUPAD,
+                                                 Value = i.KULUPID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniOgrenci(TBLOGRENCILER p3)
+        {
+            var klp = db.TBLKULUPLER.Where(m => m.KULUPID == p3.TBLKULUPLER.KULUPID).FirstOrDefault();
+            p3.TBLKULUPLER = klp;
+            db.TBLOGRENCILER.Add(p3);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var ogr = db.TBLOGRENCILER.Find(id);
+            db.TBLOGRENCILER.Remove(ogr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult OgrenciGetir(int id)
+        {
+            var ogrenci = db.TBLOGRENCILER.Find(id);
+            return View("OgrenciGetir", ogrenci);
+        }
+
+        public ActionResult Guncelle(TBLOGRENCILER p)
+        {
+            var ogr = db.TBLOGRENCILER.Find(p.OGRENCIID);
+            ogr.OGRAD = p.OGRAD;
+            ogr.OGSOYAD = p.OGSOYAD;
+            ogr.OGRFOTOGRAF = p.OGRFOTOGRAF;
+            ogr.OGRCINSIYET = p.OGRCINSIYET;
+            ogr.OGRKULUP = p.OGRKULUP;
+            db.SaveChanges();
+            return RedirectToAction("Index","Ogrencı");
+        }
+
+
+    } 
+}
+
+//List<SelectListItem> items = new List<SelectListItem>();
+
+//items.Add(new SelectListItem { Text = "Matematik", Value = "0" });
+
+//items.Add(new SelectListItem { Text = "Fen Bilgisi", Value = "1" });
+
+//items.Add(new SelectListItem { Text = "Atatürk İlke ve İnkilapları", Value = "2", Selected = true });
+
+//items.Add(new SelectListItem { Text = "Coğrafya", Value = "3" });
+
+//items.Add(new SelectListItem { Text = "Edebiyat", Value = "4" });
+
+//ViewBag.DersAd = items;
+
+//return View();
